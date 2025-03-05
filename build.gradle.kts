6d2de1f12e9b4c12077372b71823a0c29e3b76a4
Joao <joaogabedev@gmail.com>
2025-03-04 23:49:49 -0300

    Feat: Gradle final setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,16 +27,20 @@ configurations {
 repositories {
 	mavenCentral()
 }
-
+var mapStructVersion = "1.6.3"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("org.mapstruct:mapstruct:$mapStructVersion")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
+
+	annotationProcessor("org.mapstruct:mapstruct-processor:$mapStructVersion")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
