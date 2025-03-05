1a7b90df25e2f546b7f284ca130c0a18c59e24d9
Joao <joaogabedev@gmail.com>
2025-03-04 23:49:14 -0300

    Feat: Add Response Class

---
+++ src/main/java/com/example/barbershopapi/controller/response/ScheduleAppointmentMonthResponse.java
@@ -0,0 +1,15 @@
+package com.example.barbershopapi.controller.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record ScheduleAppointmentMonthResponse(
+        @JsonProperty("year")
+        int year,
+        @JsonProperty("month")
+        int month,
+        List<ClientScheduleAppointmentResponse> scheduledAppointments
+) {
+
+}
